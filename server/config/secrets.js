e0ddd4c2a73c6b7fb319bfea7655ff5c4c8ec0bc
Jesse Pollak <jpollak92@gmail.com>
2015-10-16 13:24:21 -0700

    change database url

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Find the appropriate database to connect to, default to localhost if not found.
-  db: process.env.POSTGRES_URL || 'postgres://:@localhost:5432/donotpay',
+  db: process.env.DATABASE_URL || 'postgres://:@localhost:5432/donotpay',
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
   jsonWebTokenSecret: process.env.JSON_WEB_TOKEN_SECRET || 'Your JSON web token secret goes here',
   clefAppID: process.env.CLEF_APP_ID || "d5c497d85f986be65471108f722d2a0a",
